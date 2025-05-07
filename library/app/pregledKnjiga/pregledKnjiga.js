5879f2941fb94a34c772a63ddbf15a0e3fd6d037
Josip Valtner <josipvaltner@gmail.com>
2025-05-07 15:12:16 +0200

    Napravljena osnovna struktura fajlova i dir i napravljene klase

---
+++ library/app/pregledKnjiga/pregledKnjiga.js
@@ -0,0 +1,13 @@
+'use strict'
+class Knjiga{
+    constructor(id, naziv, datumStampanja,url,opis,popularnost, iznajmljena){
+        this.id = id
+        this.naziv = naziv
+        this.datumStampanja = datumStampanja
+        this.url = url
+        this.opis = opis
+        this.popularnost = popularnost
+        this.iznajmljena = iznajmljena
+    }
+
+}

5cc1ddc22702750ea574e25c0648de015db60da2
Josip Valtner <josipvaltner@gmail.com>
2025-05-07 18:29:30 +0200

    izmene

--- library/app/mojeKnjige/mojeKnjige.js
+++ library/app/mojeKnjige/mojeKnjige.js
@@ -1,6 +1,6 @@
 'use strict'
 class Knjiga{
-    constructor(id, naziv, datumStampanja,url,opis,popularnost,iznajmljena){
+    constructor(id, naziv, datumStampanja,url,opis,popularnost, iznajmljena){
         this.id = id
         this.naziv = naziv
         this.datumStampanja = datumStampanja
@@ -11,3 +11,57 @@ class Knjiga{
     }
 
 }
+document.addEventListener('DOMContentLoaded', initializeKnjige)
+
+function initializeKnjige() {
+    let knjige = []
+    
+    let knjigeString = localStorage.getItem("knjige")
+    if (knjigeString) {
+        knjige = JSON.parse(knjigeString)
+    }
+
+    createProductRows(knjige)
+    saveLocalStorage(knjige)
+}
+
+
+function saveLocalStorage(knjige) {
+    let jelaJSON = JSON.stringify(knjige)
+    localStorage.setItem("knjige", jelaJSON)
+}
+function createProductRows(knjige) {
+    let table = document.querySelector("pregled")
+    let tabela = document.querySelector("#pregledBody")
+    tabela.innerHTML = ''
+
+    for (let knjiga of knjige) {
+        let tr = document.createElement("tr")
+        let br = document.createElement("td")
+        let naziv = document.createElement("td")
+        let opcija = document.createElement("td")
+        let opcijaBtn = document.createElement("button")
+
+        br.textContent = knjiga.id
+        naziv.textContent = knjiga.naziv
+        opcija.appendChild(opcijaBtn)
+        opcijaBtn.id = "opcijaBtn"
+        opcijaBtn.name = "opcijaBtn"
+        opcijaBtn.value = "Vrati"
+        opcijaBtn.textContent ="Vrati"
+
+        tr.appendChild(br)
+        tr.appendChild(naziv)
+        tr.appendChild(opcija)
+        opcijaBtn.addEventListener('click',
+            function (event) {
+                handleReturnBook(knjiga,knjige)
+                event.stopPropagation()
+            })
+        tabela.appendChild(tr)
+    }
+}
+function handleReturnBook(knjiga,knjige){
+    
+}
+
